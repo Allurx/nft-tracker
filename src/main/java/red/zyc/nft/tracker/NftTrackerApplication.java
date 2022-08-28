d8c0bac5874853ed8aaf9102e529cf049ad27b07
zyc <1343786337@qq.com>
2022-08-28 22:23:12 +0800

    Initial commit

---
+++ src/main/java/red/zyc/nft/tracker/NftTrackerApplication.java
@@ -0,0 +1,13 @@
+package red.zyc.nft.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/red/zyc/nft/tracker/NftTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package red.zyc.nft.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
