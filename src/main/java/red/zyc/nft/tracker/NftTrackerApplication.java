de8c669c0fad6caabc79fe2b0b1a9c74dedd0df4
zyc <1343786337@qq.com>
2022-08-28 22:24:58 +0800

    代码优化

--- src/main/java/red/zyc/nft/tracker/NftTrackerApplication.java
+++ src/main/java/red/zyc/nft/tracker/NftTrackerApplication.java
@@ -3,6 +3,9 @@ package red.zyc.nft.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zyc
+ */
 @SpringBootApplication
 public class NftTrackerApplication {
 
